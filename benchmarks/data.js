0429f8585e6f38de1f956a647fa0ab8716cb42ff
github-action-benchmark <github@users.noreply.github.com>
2025-11-06 23:47:55 +0000

    add n8n-mcp Benchmarks (customSmallerIsBetter) benchmark result for 3d5ceae43faeba4e9954f03f42cffdf0b38e0d35

---
+++ benchmarks/data.js
@@ -0,0 +1,39 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1762472875601,
+  "repoUrl": "https://github.com/Chekosworld/n8n-mcp",
+  "entries": {
+    "n8n-mcp Benchmarks": [
+      {
+        "commit": {
+          "author": {
+            "email": "56956555+czlonkowski@users.noreply.github.com",
+            "name": "czlonkowski",
+            "username": "czlonkowski"
+          },
+          "committer": {
+            "email": "56956555+czlonkowski@users.noreply.github.com",
+            "name": "czlonkowski",
+            "username": "czlonkowski"
+          },
+          "distinct": true,
+          "id": "3d5ceae43faeba4e9954f03f42cffdf0b38e0d35",
+          "message": "updated date",
+          "timestamp": "2025-11-06T00:21:41+01:00",
+          "tree_id": "a2dc846eb1b561f35609277f1fb3aaeb54bdb442",
+          "url": "https://github.com/Chekosworld/n8n-mcp/commit/3d5ceae43faeba4e9954f03f42cffdf0b38e0d35"
+        },
+        "date": 1762472874706,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "sample - array sorting - small",
+            "value": 0.0136,
+            "range": "0.3096",
+            "unit": "ms",
+            "extra": "73341 ops/sec"
+          }
+        ]
+      }
+    ]
+  }
+}
