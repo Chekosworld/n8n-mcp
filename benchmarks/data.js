bd75309e784ab9a1fe07c8797cc711733792ec8d
github-action-benchmark <github@users.noreply.github.com>
2025-11-07 20:10:17 +0000

    add n8n-mcp Benchmarks (customSmallerIsBetter) benchmark result for 0e0f0998af7fa8dd60fc014fbc0e99be8bc2c5ff

--- benchmarks/data.js
+++ benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1762472875601,
+  "lastUpdate": 1762546217639,
   "repoUrl": "https://github.com/Chekosworld/n8n-mcp",
   "entries": {
     "n8n-mcp Benchmarks": [
@@ -23,6 +23,37 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/Chekosworld/n8n-mcp/commit/3d5ceae43faeba4e9954f03f42cffdf0b38e0d35"
         },
         "date": 1762472874706,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "sample - array sorting - small",
+            "value": 0.0136,
+            "range": "0.3096",
+            "unit": "ms",
+            "extra": "73341 ops/sec"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "56956555+czlonkowski@users.noreply.github.com",
+            "name": "Romuald Członkowski",
+            "username": "czlonkowski"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "0e0f0998af7fa8dd60fc014fbc0e99be8bc2c5ff",
+          "message": "Merge pull request #403 from czlonkowski/feat/workflow-activation-operations",
+          "timestamp": "2025-11-07T07:54:33+01:00",
+          "tree_id": "150a25e99e1083077a438e5281e6514f8ca7c2b1",
+          "url": "https://github.com/Chekosworld/n8n-mcp/commit/0e0f0998af7fa8dd60fc014fbc0e99be8bc2c5ff"
+        },
+        "date": 1762546217396,
         "tool": "customSmallerIsBetter",
         "benches": [
           {
